7b5f8bafa255b7dacb4362f010fc0ed7ccea13cf
Faith Chepkoech Birgen <faybirgen815@gmail.com>
2024-12-06 16:35:04 +0300

    add solutions on index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+//write your code here
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';    
+}
+
+
+function overwriteBestCustomer(){
+    window.bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'nellie';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob';
+}
+changeLeastFavoriteCustomer();
+console.log(changeLeastFavoriteCustomer());
